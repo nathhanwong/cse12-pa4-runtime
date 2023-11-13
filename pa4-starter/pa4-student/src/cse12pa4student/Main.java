973c1d41e93a9ca4caf1ca654f07843e8e5ad2d9
nathhanwong <wongnathan17@gmail.com>
2023-11-13 11:57:32 -0800

    cse12pa4
    cse12pa4 update

--- pa4-starter/pa4-student/src/cse12pa4student/Main.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Main.java
@@ -1,7 +1,16 @@
 package cse12pa4student;
+import java.util.List;
 public class Main {
 	
 	public static void main(String[] args) {
+		String[]toRun={"C","D"};
+		int startN = 40;
+		int stopN = 100;
+
+		List<Measurement> measurements= Measure.measure(toRun,startN,stopN);
+
+		String csvData = Measure.measurementsToCSV(measurements);
+		System.out.println(csvData);
 		
 	}
 }
--- pa4-starter/pa4-student/src/cse12pa4student/Measure.java
+++ pa4-starter/pa4-student/src/cse12pa4student/Measure.java
@@ -1,25 +1,50 @@
 package cse12pa4student;
 
 import java.util.List;
+import java.util.ArrayList;
 import static cse12pa4mysteries.Mysteries.*;
 
 public class Measure {
 
 
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
-		/** TODO **/
-		
-		//Example call to mystery method
-		mysteryA(50);
-		return null;
+		List<Measurement>measurements = new ArrayList<>();
+
+		for (String methodName : toRun) {
+			for (int n = startN; n <= stopN; n++) {
+				long startTime = System.nanoTime();
+
+				switch (methodName) {
+					case "A":
+					mysteryA(n);
+					break;
+					case "B":
+					mysteryB(n);
+					break;
+				}
+				long endTime = System.nanoTime();
+				long executionTime= endTime - startTime;
+
+				measurements.add(new Measurement(methodName, n, executionTime));
+			}
+		}
+		return measurements;
 	}
 	
 
 	public static String measurementsToCSV(List<Measurement> toConvert) {
-		/** TODO **/
-		return null;
+		StringBuilder csvBuilder = new StringBuilder();
+
+		csvBuilder.append("name, n, nanoseconds\n");
+		for (Measurement measurement : toConvert) {
+			csvBuilder.append(measurement.name)
+			.append(",")
+			.append(measurement.valueOfN)
+			.append(",")
+			.append(measurement.nanosecondsToRun)
+			.append("\n");
+		}
+		return csvBuilder.toString();
+
 	}
-	
-	/* Add any helper methods you find useful */
-		
 }
